97f10aa502b67a527249e89d654f53eb11782449
k80Unbexb <k80Unbexb@dxb53c4.local>
2017-09-28 22:18:21 -0700

    plmap3 package with support for multi-threading : python3

--- plmap3/__init__.py
+++ plmap3/__init__.py
@@ -1,2 +1,1 @@
 from plmap3.plmapt import plmapt
-from plmap3.plmapp import plmapp
--- setup.py
+++ setup.py
@@ -4,15 +4,10 @@ from setuptools import setup
 
 setup(name='plmap3',
       version='1.0.0',
-      description='Parallel map using multi-threading and multi-processing for python3',
-      author='Lohith B R',
+      description='Parallel map using multi-threading for python3',
+      author='Coding Van Gogh',
       packages=['plmap3'],
-      license = "MIT",
-      keywords = "parallelization plmap plmapp plmapt pmapp multi-threading multi-processing parallel map python3",
-      url = "https://github.com/brlohith/parallelization3",
-    #   install_requires=[
-    #       'queue',
-    #   ]
-     )
-
-
+      license="MIT",
+      keywords="parallelization plmap  plmapt  multi-threading  parallel map python3",
+      url="https://github.com/CodingVanGogh/parallelization3"
+      )
